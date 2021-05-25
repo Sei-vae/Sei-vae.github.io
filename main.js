c07e1b0adf380d475f9d83186dc7368791033380
Sei <iamsei97@gmail.com>
2021-05-25 10:07:53 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 import './style.css'
 
 //import * as THREE from 'three';
-import * as THREE from './three';
+//import * as THREE from './three';
 
-import { OrbitControls } from './three/examples/jsm/controls/OrbitControls';
+//import { OrbitControls } from './three/examples/jsm/controls/OrbitControls';
 
-import { GLTFLoader } from './three/examples/jsm/loaders/GLTFLoader.js';
+//import { GLTFLoader } from './three/examples/jsm/loaders/GLTFLoader.js';
+
+
+import * as THREE from 'https://threejs.org/build/three.module.js';
+import { OrbitControls } from 'https://threejs.org/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'https://threejs.org/examples/jsm/loaders/GLTFLoader.js';
+		
+
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/ window.innerHeight, 0.1, 1000);
